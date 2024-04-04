c0f54b1b6b3d36351b5f8f3ef4e755dd6e3fdfe5
danbiondolillo23 <danbiondolillo23@gmail.com>
2024-04-04 06:15:02 -0400

    change file path for hosting

--- PourProController.php
+++ PourProController.php
@@ -76,31 +76,37 @@ class PourProController {
         if (!empty($this->errorMessage)) {
             $errorMessage = "<div class='alert alert-danger'>{$this->errorMessage}</div>";
         }
-        include '/opt/src/pourpro/templates/login.php';
+        // include '/opt/src/pourpro/templates/login.php';
         // include '/students/jpg5wq/students/jpg5wq/private/pourpro/templates/login.php';
-        // include '/students/xtz3mx/students/xtz3mx/private/pourpro/templates/home.php';
+        include '/students/xtz3mx/students/xtz3mx/private/pourpro/templates/login.php';
     }
 
     public function showSignUp() {
-        include '/opt/src/pourpro/templates/signup.php';
+        // include '/opt/src/pourpro/templates/signup.php';
         // include '/students/jpg5wq/students/jpg5wq/private/pourpro/templates/signup.php';
-        // include '/students/xtz3mx/students/xtz3mx/private/pourpro/templates/signup.php';
+        include '/students/xtz3mx/students/xtz3mx/private/pourpro/templates/signup.php';
+    }
+
+    public function showProfile(){
+        // include '/opt/src/pourpro/templates/profile.php';
+        // include '/students/jpg5wq/students/jpg5wq/private/pourpro/templates/profile.php';
+        include '/students/xtz3mx/students/xtz3mx/private/pourpro/templates/profile.php';
     }
 
     public function showInventory() {
         $this->getAllProducts();
-        include '/opt/src/pourpro/templates/inventory.php';
+        // include '/opt/src/pourpro/templates/inventory.php';
         // include '/students/jpg5wq/students/jpg5wq/private/pourpro/templates/inventory.php';
-        // include '/students/xtz3mx/students/xtz3mx/private/pourpro/templates/inventory.php';
+        include '/students/xtz3mx/students/xtz3mx/private/pourpro/templates/inventory.php';
     }
 
     public function showDetail($product_id) {
         $productDetails = $this->getProductDetails($product_id);
         $_SESSION['product_details'] = $productDetails;
 
-        include '/opt/src/pourpro/templates/detail.php';
+        // include '/opt/src/pourpro/templates/detail.php';
         // include '/students/jpg5wq/students/jpg5wq/private/pourpro/templates/detail.php';
-        // include '/students/xtz3mx/students/xtz3mx/private/pourpro/templates/detail.php';
+        include '/students/xtz3mx/students/xtz3mx/private/pourpro/templates/detail.php';
     }
 
     private function getProductDetails($product_id) {
@@ -394,12 +400,6 @@ class PourProController {
         $products = $this->db->query("select * from products");
         $_SESSION["products"] = $products;
         return $products;
-    }
-
-    public function showProfile(){
-        include '/opt/src/pourpro/templates/profile.php';
-        // include '/students/jpg5wq/students/jpg5wq/private/pourpro/templates/profile.php';
-        // include '/students/xtz3mx/students/xtz3mx/private/pourpro/templates/profile.php';
     }
 
     public function logout() {
